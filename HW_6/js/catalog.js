255eb7a5d81290eaf57565f70b9426085c17214d
Gurin Artem <31992338+ghostnastromo@users.noreply.github.com>
2019-12-11 15:35:03 +0200

    Обнаружил ошибку, но не смог решить проблему(

---
+++ HW_6/js/catalog.js
@@ -0,0 +1,6 @@
+var productsCatalog = [
+	{id:'el1', name:'Canon EOS R',         price:10000, img:'https://images.unsplash.com/photo-1480365334925-2aee561aa28e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=773&q=80'},
+	{id:'el2', name:'Canon EOS 650D',      price:20000, img:'https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'},
+	{id:'el3', name:'Canon EOS 5D Mark',   price:30000, img:'https://images.unsplash.com/photo-1518890414976-bb41382be43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'},
+	{id:'el4', name:'Sony Cyber-shot DSC', price:40000, img:'https://images.unsplash.com/photo-1500630417200-63156e226754?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'}
+];
